9e1445fd5dad52a90e05ac409d334d81f9ca7a57
TalhaAkgul <72688377+TalhaAkgul@users.noreply.github.com>
2021-05-16 11:58:01 +0300

    Update method2.py

--- method2.py
+++ method2.py
@@ -15,21 +15,27 @@ import numpy as np
 optimum_course_centers = []
 f = open("test2.txt", "r")
 N = int(f.readline()) 
-max_dist,min_fee,max_fee = f.readline().split(" ")
+max_dist,min_fee,max_fee = f.readline().split(" ") #Getting boundaries
 max_dist,min_fee,max_fee = int(max_dist),int(min_fee),int(max_fee)
-size = max_fee-min_fee
-course_centers = np.zeros(size+1)
+size = max_fee-min_fee+1 #size of array
+course_centers = np.zeros(size)
 
 for i in range(N):
     dist,fee = f.readline().split(" ")
     dist,fee = int(dist),int(fee)
+    #if it is in the range
     if dist <= max_dist and fee <= max_fee and fee >= min_fee: 
+        #if there is an element in that index that means there are two elements with the same fee,
+        #then we know that the one with the shorter distance should stay in the selected course centers list
         if course_centers[fee-min_fee] > dist or course_centers[fee-min_fee] == 0: 
             course_centers[fee-min_fee] = dist
 j = 0
+#we know that some indexes may be empty if there is no center with that fee
 while course_centers[j] == 0: j += 1 # find the first non-empty distance
 current_dist = course_centers[j]
 optimum_course_centers.append((current_dist,j+min_fee))
+
+#comparing last selected item with the next item to decide whether it can be selected or not
 for i in range(j,len(course_centers)):
     if course_centers[i] == 0: continue
     fee = i + min_fee
@@ -38,7 +44,7 @@ for i in range(j,len(course_centers)):
         optimum_course_centers.append((dist, fee))
         current_dist = dist
         current_fee = fee
-#course_centers[8] = ((0,0))
+
 print(course_centers)
 print("OPTIMUM COURSE CENTERS")
 print(optimum_course_centers)
